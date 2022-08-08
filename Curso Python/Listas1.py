6ce894d5164d51ca79507d542ca8a568020557c5
Blaviken <jarellan@cuc.edu.co>
2022-08-08 12:46:25 -0500

    Update

---
+++ Curso Python/Listas1.py
@@ -0,0 +1,14 @@
+A =[1,2,3,4]
+print(A)
+
+A =[1,2,3,4,"python",4.6]
+print(A)
+
+A =[1,2,3,4,"python",4.6]
+print(A[2])
+
+A =[1,2,3,4,"python",4.6]
+print(A[4])
+
+A =[1,2,3,4,"python",4.6]
+print(A[5])
