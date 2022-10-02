03e05a909a8a10dc86132978a0f8afe083516036
Blaviken <jarellan@cuc.edu.co>
2022-10-02 11:03:42 -0500

    2/10/2022

---
+++ Corte 2/Borraddor_Clase3.py
@@ -0,0 +1,201 @@
+a="\n"
+class m:
+
+    def __init__(self):
+        self.m1=[]
+        self.m2=[]
+
+    def Setm1(self,m1:list):
+        self.m1=m1
+    
+    def Getm1(self):
+        return self.m1
+
+    def Setm2(self,m2:list):
+        self.m2=m2
+    
+    def Getm2(self):
+        return self.m2
+
+    def llenar_matriz1(self,filas:int, columnas:int):
+        for i in range (filas):
+            self.m1.append([])
+            for j in range (columnas):
+                valor1=int(input("Ingrese un elemento: "))
+                self.m1[i].append(valor1)
+        print("Proceso Exitoso....")
+
+    def mostrar_matriz1(self):
+        for i in range(len(self.m1)):
+            for j in range(len(self.m1[0])):
+                dato=self.m1[i][j]
+                print(dato)
+
+    def llenar_matriz2(self,filas:int, columnas:int):
+        for i in range (filas):
+            self.m2.append([])
+            for j in range (columnas):
+                valor2=int(input("Ingrese un elemento: "))
+                self.m2[i].append(valor2)
+        print("Proceso Exitoso....")
+
+    def mostrar_matriz2(self):
+        for i in range(len(self.m2)):
+            for j in range(len(self.m2[0])):
+                dato=self.m2[i][j]
+                print(dato)
+
+    def diagonal_principal_secundaria1(self):
+        j=0
+        sumato_prin=0
+        sumato_secu=0
+        for i in range(len(self.m1)):
+            sumato_secu=sumato_secu+self.m1[i][len(self.m1[0])- 1 -j]
+            j=j+1
+            for j in range(len(self.m1[0])):
+                if j == i:
+                    sumato_prin=sumato_prin+self.m1[i][j]
+        print(" ")
+        print(f"Sumatoria de diagonal principal: {sumato_prin}")
+        print(" ")
+        print(f"Sumatoria de diagonal secundaria: {sumato_secu}")
+        print(" ")
+        if sumato_prin > sumato_secu:
+            print("La diagonal principal es mayor")
+            print(" ")
+            print("La diagonal secundaria es menor")
+            print(" ")
+        elif sumato_prin < sumato_secu:
+            print("La diagonal secundaria es mayor")
+            print(" ")
+            print("La diagonal principal es menor")
+            print(" ")
+        else:
+            print("Las dos diagonales tienen el mismo valor")
+            print(" ")
+
+    def diagonal_principal_secundaria2(self):
+        j=0
+        sumato_prin=0
+        sumato_secu=0
+        for i in range(len(self.m2)):
+            sumato_secu=sumato_secu+self.m2[i][len(self.m2[0])- 1 -j]
+            j=j+1
+            for j in range(len(self.m2[0])):
+                if j == i:
+                    sumato_prin=sumato_prin+self.m2[i][j]
+        print(" ")
+        print(f"Sumatoria de diagonal principal: {sumato_prin}")
+        print(" ")
+        print(f"Sumatoria de diagonal secundaria: {sumato_secu}")
+        print(" ")
+        if sumato_prin > sumato_secu:
+            print("La diagonal principal es mayor")
+            print(" ")
+            print("La diagonal secundaria es menor")
+            print(" ")
+        elif sumato_prin < sumato_secu:
+            print("La diagonal secundaria es mayor")
+            print(" ")
+            print("La diagonal principal es menor")
+            print(" ")
+        else:
+            print("Las dos diagonales tienen el mismo valor")
+            print(" ")
+
+        
+    
+class Operaciones:
+
+    """def suma_matriz(self):
+        print(a)
+        print("SUMA")
+        filas = len(self.m1().getMatriz())
+        columnas = len(self.m1().getMatriz()[0])
+        matriz3 = []
+        for i in range(filas):
+            matriz3.append([])
+            for j in range(columnas):
+                x = self.getm1().getMatriz()[i][j] + self.getm2().getMatriz()[i][j]
+                matriz3[i].append(x)
+        for i in range(filas):
+            print("[", end=" ")
+            for j in range(columnas):
+                if j != columnas - 1:
+                    print(f"{matriz3[i][j]}", end=" ")
+                else:
+                    print(f"{matriz3[i][j]}", end=" ")
+            print("]")"""
+
+    
+
+def main():
+
+    matriz1=m()
+    filas=int(input("Ingrese el numero de filas: "))
+    columnas=int(input("Ingrese el numero de columnas: "))
+    matriz1.llenar_matriz1(filas,columnas)
+    matriz1.mostrar_matriz1()
+    matriz1.diagonal_principal_secundaria1()
+
+    matriz2=m()
+    filas=int(input("Ingrese el numero de filas: "))
+    columnas=int(input("Ingrese el numero de columnas: "))
+    matriz2.llenar_matriz2(filas,columnas)
+    matriz2.mostrar_matriz2()
+    matriz2.diagonal_principal_secundaria2()
+
+
+
+main()
+
+print(a)
+print("By Julio Arellano @ Blaviken-fsociety")
+print(a)
+
+"""    # División
+    def division(self):
+        print("")
+        print("DIVISIÓN")
+        filas = len(self.getParte1().getMatriz())
+        columnas = len(self.getParte1().getMatriz()[0])
+        matriz_nueva_1 = []
+        matriz_nueva_2 = []
+        dividendo_1 = int(input("Ingrese el dividendo para parte 1: "))
+        dividendo_2 = int(input("Ingrese el dividendo para parte 2: "))
+
+        while dividendo_1 == 0 or dividendo_2 == 0:
+            print("Datos no válidos...")
+            if dividendo_1 == 0:
+                dividendo_1 = int(input("Ingrese el dividendo para parte 1: "))
+            if dividendo_2 == 0:
+                dividendo_2 = int(input("Ingrese el dividendo para parte 2: ")) 
+        
+        for i in range(filas):
+            matriz_nueva_1.append([])
+            matriz_nueva_2.append([])
+            for j in range(columnas):
+                x = round(self.getParte1().getMatriz()[i][j] / dividendo_1, 3)
+                y = round(self.getParte2().getMatriz()[i][j] / dividendo_2, 3)
+                matriz_nueva_1[i].append(x)
+                matriz_nueva_2[i].append(y)
+        
+        print(f"Primera matriz dividida entre: {dividendo_1}")
+        for i in range(filas):
+                print("[", end="")
+                for j in range(columnas):
+                    if j != columnas - 1:
+                        print(f"{matriz_nueva_1[i][j]}", end=" ")
+                    else:
+                        print(f"{matriz_nueva_1[i][j]}", end="")
+                print("]")
+
+        print(f"Segunda matriz dividida entre: {dividendo_2}")
+        for i in range(filas):
+                print("[", end="")
+                for j in range(columnas):
+                    if j != columnas - 1:
+                        print(f"{matriz_nueva_2[i][j]}", end=" ")
+                    else:
+                        print(f"{matriz_nueva_2[i][j]}", end="")
+                print("]")"""
--- Corte 2/Clase3.py
+++ Corte 2/Clase3.py
@@ -1,155 +1,304 @@
-
 a="\n"
 
-
-
-class m:
-
+class Matrices():
     def __init__(self):
-        self.m1=[]
-        self.m2=[]
+        self.__matriz = []
 
-    def Setm1(self,m1:list):
-        self.m1=m1
-    
-    def Getm1(self):
-        return self.m1
+    def setMatriz(self, x:list):
+        self.__matriz = x
 
-    def Setm2(self,m2:list):
-        self.m2=m2
-    
-    def Getm2(self):
-        return self.m2
+    def getMatriz(self):
+        return self.__matriz
 
-    def llenar_matriz1(self,filas:int, columnas:int):
-        for i in range (filas):
-            self.m1.append([])
-            for j in range (columnas):
-                valor1=int(input("Ingrese un elemento: "))
-                self.m1[i].append(valor1)
-        print("Proceso Exitoso....")
-
-    def mostrar_matriz1(self):
-        for i in range(len(self.m1)):
-            for j in range(len(self.m1[0])):
-                dato=self.m1[i][j]
-                print(dato)
-
-    def llenar_matriz2(self,filas:int, columnas:int):
-        for i in range (filas):
-            self.m2.append([])
-            for j in range (columnas):
-                valor2=int(input("Ingrese un elemento: "))
-                self.m2[i].append(valor2)
-        print("Proceso Exitoso....")
-
-    def mostrar_matriz2(self):
-        for i in range(len(self.m2)):
-            for j in range(len(self.m2[0])):
-                dato=self.m2[i][j]
-                print(dato)
-
-    def diagonal_principal_secundaria1(self):
-        j=0
-        sumato_prin=0
-        sumato_secu=0
-        for i in range(len(self.m1)):
-            sumato_secu=sumato_secu+self.m1[i][len(self.m1[0])- 1 -j]
-            j=j+1
-            for j in range(len(self.m1[0])):
-                if j == i:
-                    sumato_prin=sumato_prin+self.m1[i][j]
-        print(" ")
-        print(f"Sumatoria de diagonal principal: {sumato_prin}")
-        print(" ")
-        print(f"Sumatoria de diagonal secundaria: {sumato_secu}")
-        print(" ")
-        if sumato_prin > sumato_secu:
-            print("La diagonal principal es mayor")
-            print(" ")
-            print("La diagonal secundaria es menor")
-            print(" ")
-        elif sumato_prin < sumato_secu:
-            print("La diagonal secundaria es mayor")
-            print(" ")
-            print("La diagonal principal es menor")
-            print(" ")
-        else:
-            print("Las dos diagonales tienen el mismo valor")
-            print(" ")
-
-    def diagonal_principal_secundaria2(self):
-        j=0
-        sumato_prin=0
-        sumato_secu=0
-        for i in range(len(self.m2)):
-            sumato_secu=sumato_secu+self.m2[i][len(self.m2[0])- 1 -j]
-            j=j+1
-            for j in range(len(self.m2[0])):
-                if j == i:
-                    sumato_prin=sumato_prin+self.m2[i][j]
-        print(" ")
-        print(f"Sumatoria de diagonal principal: {sumato_prin}")
-        print(" ")
-        print(f"Sumatoria de diagonal secundaria: {sumato_secu}")
-        print(" ")
-        if sumato_prin > sumato_secu:
-            print("La diagonal principal es mayor")
-            print(" ")
-            print("La diagonal secundaria es menor")
-            print(" ")
-        elif sumato_prin < sumato_secu:
-            print("La diagonal secundaria es mayor")
-            print(" ")
-            print("La diagonal principal es menor")
-            print(" ")
-        else:
-            print("Las dos diagonales tienen el mismo valor")
-            print(" ")
-
-        
-    
-class Operaciones:
-
-    """def suma_matriz(self):
+    def llenarMatriz(self, indice:str, filas:int, columnas:int):
         print(a)
-        print("SUMA")
-        filas = len(self.m1().getMatriz())
-        columnas = len(self.m1().getMatriz()[0])
+        print(f"Ingrese los datos de la matriz {indice}")
+        for i in range(filas):
+            self.getMatriz().append([])
+            for j in range(columnas):
+                valor = int(input("Ingrese un elemento: "))
+                self.getMatriz()[i].append(valor)
+
+    def mostrarMatriz(self, indice:str):
+        print(a)
+        print(f"Matriz {indice}")
+        filas = len(self.getMatriz())
+        columnas = len(self.getMatriz()[0])
+        for i in range(filas):
+            print("[", end="")
+            for j in range(columnas):
+                if j != columnas - 1:
+                    print(f"{self.getMatriz()[i][j]}", end=" ")
+                else:
+                    print(f"{self.getMatriz()[i][j]}", end="")
+            print("]")
+          
+class Operadoraiones():
+    def __init__(self):
+        self.__matriz1 = Matrices()
+        self.__matriz2 = Matrices()
+
+    def setParte1(self, x:Matrices()):
+        self.__matriz1 = x
+    
+    def getParte1(self):
+        return self.__matriz1
+    
+    def setParte2(self, x:Matrices()):
+        self.__matriz2 = x
+    
+    def getParte2(self):
+        return self.__matriz2
+
+    def Comparar_Diagonal_Principal(self):
+        print(a)
+        print("Comparamos la diagonal principal de ambas matrices: ")
+        print(a)
+
+        filas = len(self.getParte1().getMatriz())
+        columnas = len(self.getParte1().getMatriz()[0])
+        parte1 = []
+        parte2 = []
+        for i in range(filas):
+            for j in range(columnas):
+                if i == j:
+                    parte1.append(self.getParte1().getMatriz()[i][j])
+                    parte2.append(self.getParte2().getMatriz()[i][j])
+
+        mayor_parte1 = parte1[0]
+        mayor_parte2 = parte2[0]
+        menor_parte1 = parte1[0]
+        menor_parte2 = parte2[0]
+
+        for i in range(len(parte1)):
+            if parte1[i] > mayor_parte1:
+                mayor_parte1 = parte1[i]
+            if parte1[i] < menor_parte1:
+                menor_parte1 = parte1[i]
+
+            if parte2[i] > mayor_parte2:
+                mayor_parte2 = parte2[i]
+            if parte2[i] < menor_parte2:
+                menor_parte2 = parte2[i]
+        
+        sum_parte1 = 0
+        sum_parte2 = 0
+        for i in range(len(parte1)):
+            sum_parte1 = sum_parte1 + parte1[i]
+            sum_parte2 = sum_parte2 + parte2[i]
+        
+        if len(parte1) == 0:
+            prom_parte1 = 0
+        else:
+            prom_parte1 = sum_parte1 / len(parte1)
+
+        if len(parte2) == 0:
+            prom_parte2 = 0
+        else:
+            prom_parte2 = sum_parte2 / len(parte2)
+
+        print(f"Dígito mayor en la matriz 1: {mayor_parte1}")
+        print(f"Dígito mayor en la matriz 2: {mayor_parte2}")
+        print(f"Dígito menor en la matriz 1: {menor_parte1}")
+        print(f"Dígito menor en la matriz 2: {menor_parte2}")
+        print(f"Promedio en la matriz 1: {prom_parte1}")
+        print(f"Promedio en la matriz 2: {prom_parte2}")
+
+    def Comparar_Elementos_de_Matrices(self):
+        print(a)
+        print("Comparamos los elementos de ambas matrices: ")
+        print(a)
+
+        filas = len(self.getParte1().getMatriz())
+        columnas = len(self.getParte1().getMatriz()[0])
+        mayor_parte1 = self.getParte1().getMatriz()[0][0]
+        mayor_parte2 = self.getParte2().getMatriz()[0][0]
+        menor_parte1 = self.getParte1().getMatriz()[0][0]
+        menor_parte2 = self.getParte2().getMatriz()[0][0]
+        for i in range(filas):
+            for j in range(columnas):
+                if self.getParte1().getMatriz()[i][j] > mayor_parte1:
+                    mayor_parte1 = self.getParte1().getMatriz()[i][j]
+                if self.getParte1().getMatriz()[i][j] < menor_parte1:
+                    menor_parte1 = self.getParte1().getMatriz()[i][j]
+
+                if self.getParte2().getMatriz()[i][j] > mayor_parte2:
+                    mayor_parte2 = self.getParte2().getMatriz()[i][j]
+                if self.getParte2().getMatriz()[i][j] < menor_parte2:
+                    menor_parte2 = self.getParte2().getMatriz()[i][j]
+
+        sum_parte1 = 0
+        sum_parte2 = 0
+        for i in range(filas):
+            for j in range(columnas):
+                sum_parte1 = sum_parte1 + self.getParte1().getMatriz()[i][j]
+                sum_parte2 = sum_parte2 + self.getParte2().getMatriz()[i][j]
+        
+        cantidad_elementos = filas * columnas
+ 
+        if cantidad_elementos == 0:
+            prom_parte1 = 0
+        else:
+            prom_parte1 = sum_parte1 / cantidad_elementos
+
+        if cantidad_elementos == 0:
+            prom_parte2 = 0
+        else:
+            prom_parte2 = sum_parte2 / cantidad_elementos
+        
+        print(f"Dígito mayor en la matriz 1: {mayor_parte1}")
+        print(f"Dígito mayor en la matriz 2: {mayor_parte2}")
+        print(f"Dígito menor en la matriz 1: {menor_parte1}")
+        print(f"Dígito menor en la matriz 2: {menor_parte2}")
+        print(f"Promedio en la matriz 1: {prom_parte1}")
+        print(f"Promedio en la matriz 2: {prom_parte2}")
+
+    def Comparar_Diagonal_Secundaria(self):
+        print(a)
+        print("Comparamos la diagonal secundaria de ambas matrices: ")
+        print(a)
+        filas = len(self.getParte1().getMatriz())
+        columnas = len(self.getParte1().getMatriz()[0])
+        parte1 = []
+        parte2 = []
+        for i in range(filas):
+            for j in range(columnas):
+                if i == j:
+                    parte1.append(self.getParte1().getMatriz()[i][columnas - 1 - j])
+                    parte2.append(self.getParte2().getMatriz()[i][columnas - 1 - j])
+
+        mayor_parte1 = parte1[0]
+        mayor_parte2 = parte2[0]
+        menor_parte1 = parte1[0]
+        menor_parte2 = parte2[0]
+        for i in range(len(parte1)):
+            if parte1[i] > mayor_parte1:
+                mayor_parte1 = parte1[i]
+            if parte1[i] < menor_parte1:
+                menor_parte1 = parte1[i]
+
+            if parte2[i] > mayor_parte2:
+                mayor_parte2 = parte2[i]
+            if parte2[i] < menor_parte2:
+                menor_parte2 = parte2[i]
+        
+        sum_parte1 = 0
+        sum_parte2 = 0
+        for i in range(len(parte1)):
+            sum_parte1 = sum_parte1 + parte1[i]
+            sum_parte2 = sum_parte2 + parte2[i]
+        
+        if len(parte1) == 0:
+            prom_parte1 = 0
+        else:
+            prom_parte1 = sum_parte1 / len(parte1)
+
+        if len(parte2) == 0:
+            prom_parte2 = 0
+        else:
+            prom_parte2 = sum_parte2 / len(parte2)
+
+        print(f"Dígito mayor en la matriz 1: {mayor_parte1}")
+        print(f"Dígito mayor en la matriz 2: {mayor_parte2}")
+        print(f"Dígito menor en la matriz 1: {menor_parte1}")
+        print(f"Dígito menor en la matriz 2: {menor_parte2}")
+        print(f"Promedio en la matriz 1: {prom_parte1}")
+        print(f"Promedio en la matriz 2: {prom_parte2}")
+
+    def suma(self):
+        print(a)
+        print("Suma de matrices: ")
+        print(a)
+        filas = len(self.getParte1().getMatriz())
+        columnas = len(self.getParte1().getMatriz()[0])
         matriz3 = []
         for i in range(filas):
             matriz3.append([])
             for j in range(columnas):
-                x = self.getm1().getMatriz()[i][j] + self.getm2().getMatriz()[i][j]
+                x = self.getParte1().getMatriz()[i][j] + self.getParte2().getMatriz()[i][j]
                 matriz3[i].append(x)
+        
         for i in range(filas):
-            print("[", end=" ")
+            print("[", end="")
             for j in range(columnas):
                 if j != columnas - 1:
                     print(f"{matriz3[i][j]}", end=" ")
                 else:
+                    print(f"{matriz3[i][j]}", end="")
+            print("]")
+
+    def resta(self):
+        print(a)
+        print("Resta de matrices: ")
+        print(a)
+        filas = len(self.getParte1().getMatriz())
+        columnas = len(self.getParte1().getMatriz()[0])
+        matriz3 = []
+        for i in range(filas):
+            matriz3.append([])
+            for j in range(columnas):
+                x = self.getParte1().getMatriz()[i][j] - self.getParte2().getMatriz()[i][j]
+                matriz3[i].append(x)
+        
+        for i in range(filas):
+            print("[", end="")
+            for j in range(columnas):
+                if j != columnas - 1:
                     print(f"{matriz3[i][j]}", end=" ")
-            print("]")"""
+                else:
+                    print(f"{matriz3[i][j]}", end="")
+            print("]")
 
-    
+    def multi(self):
+        print(a)
+        print("Multiplicación de matrices: ")
+        print(a)
+        m1 = self.getParte1().getMatriz()
+        m2 = self.getParte2().getMatriz()
 
+        if len(m1[0]) == len(m2):
+            matriz3 = []
+            for i in range(len(m1)):
+                matriz3.append([])
+                for j in range(len(m2[0])):
+                    matriz3[i].append(0)
+
+            for i in range(len(m1)):
+                for j in range(len(m2[0])):
+
+                    for k in range(len(m1[0])):
+                        matriz3[i][j] = matriz3[i][j] + m1[i][k] * m2[k][j]
+
+            for i in range(len(matriz3)):
+                print("[", end="")
+                for j in range(len(matriz3[0])):
+                    if j != len(matriz3[0]) - 1:
+                        print(f"{matriz3[i][j]}", end=" ")
+                    else:
+                        print(f"{matriz3[i][j]}", end="")
+                print("]")
+        else:
+           print("No se pueden multiplicar") 
+        
 def main():
 
-    matriz1=m()
-    filas=int(input("Ingrese el numero de filas: "))
-    columnas=int(input("Ingrese el numero de columnas: "))
-    matriz1.llenar_matriz1(filas,columnas)
-    matriz1.mostrar_matriz1()
-    matriz1.diagonal_principal_secundaria1()
+    m1 = Matrices()
+    m1.llenarMatriz("1", 3, 3)
+    m1.mostrarMatriz("1")
 
-    matriz2=m()
-    filas=int(input("Ingrese el numero de filas: "))
-    columnas=int(input("Ingrese el numero de columnas: "))
-    matriz2.llenar_matriz2(filas,columnas)
-    matriz2.mostrar_matriz2()
-    matriz2.diagonal_principal_secundaria2()
+    m2 = Matrices()
+    m2.llenarMatriz("2", 3, 3)
+    m2.mostrarMatriz("2")
 
-
+    m3 = Operadoraiones()
+    m3.setParte1(m1)
+    m3.setParte2(m2)
+    m3.Comparar_Diagonal_Principal()
+    m3.Comparar_Elementos_de_Matrices()
+    m3.Comparar_Diagonal_Secundaria()
+    m3.suma()
+    m3.resta()
+    m3.multi()
 
 main()
 
